e0fae86c3ec0bb10ef4fdb59cb8e7faf02f3ad9c
지영은 <wlduddms97@nate.com>
2022-02-18 16:18:26 +0900

    220218 ㅍ

--- js/index.js
+++ js/index.js
@@ -1,45 +1,18 @@
 $(document).ready(function(){
-	/*헤더 over*/
-	var depth2 = $("header .gnb > li > ul");
-	var depth2_img = $("header .gnb > li > .img");
-	var gnb_btn_hide = $(".gnb_menu").css("display");
-	var slide_second = 300;
-	if(gnb_btn_hide === 'none'){
-		$("header > div").mouseover(function(){
-			$(depth2).stop().slideDown(slide_second);
-			$(depth2_img).stop().slideDown(slide_second);
-			$(".dim").stop().slideDown(slide_second);
-		}).mouseout(function(){
-			$(depth2).stop().slideUp(slide_second);
-			$(depth2_img).stop().slideUp(slide_second);
-			$(".dim").stop().slideUp(slide_second);
-		});
-	}else{
-		$("header .gnb > li > h2 > a").click(function(e){
-			e.preventDefault();
-			$(this).parents("li").siblings().children("h2").removeClass("active");
-			$(this).parent().toggleClass("active");
-			$(this).parent().siblings("ul").slideToggle(slide_second);
-			$(this).parents("li").siblings().children("ul").slideUp(slide_second)
-		});
-	}
-
-	/*모바일 메뉴*/
-	$(".gnb_menu").click(function(){
-		$("header").toggleClass("on");
-		$("nav").toggleClass("on");
-		$("body").toggleClass("on");
+	$(".protfolio .protfolio_intro .mobile").mouseover(function(){
+		var mobile_img = $(this).find("img").height();
+		var plus = 544;
+		var mobile_img_size = mobile_img-plus
+		$(this).find("img").stop().animate({marginTop: "-"+mobile_img_size}, 15000);
+	}).mouseout(function(){
+		$(this).find("img").stop().animate({marginTop: 0}, 800);
 	});
-
-	/*모바일 탑버튼*/
-	$(".m_top_btn").click(function(){
-		$("html, body").animate({"scrollTop":0},800);
+	$(".protfolio .protfolio_intro .pc").mouseover(function(){
+		var pc_img = $(this).find("img").height();
+		var plus = 800;
+		var pc_img_size = pc_img-plus
+		$(this).find("img").stop().animate({marginTop: "-"+pc_img_size}, 15000);
+	}).mouseout(function(){
+		$(this).find("img").stop().animate({marginTop: 0}, 800);
 	});
-
-	/*푸터 관련기간*/
-	$(".slide_ft .title").click(function(){
-		$(".slide_ft").toggleClass("open");
-		$(".slide_ft .cont").slideToggle();
-	});
-
 });
