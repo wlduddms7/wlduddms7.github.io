d50b2d5ff595373388ec93f549d1b83bf70b3455
지영은 <69829111+wlduddms7@users.noreply.github.com>
2022-08-23 19:53:08 +0900

    main 추가

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
 	
+		$('#fullpage').fullpage({
+			responsiveWidth: 1024,
+			'onLeave' : function (index, nextIndex, direction){
+				/* 탑버튼 섹션1에서 숨기기 */
+				if (index == 1 && direction == 'down'){
+					$(".top_btn").addClass("go_top");
+				}else if (index == 2 && direction == 'up'){
+					$(".top_btn").removeClass("go_top");
+				}
+
+				/* 푸터에서 네브 조정 */
+				if (index == 6 && direction == 'down'){
+					$(".top_btn").addClass("ft_on");
+				}else if (index == 7 && direction == 'up'){
+					$(".top_btn").removeClass("ft_on");
+				}
+			}
+
+		});
+		$('.top_btn').on('click', function(){
+			$.fn.fullpage.moveTo(1);
+			$(this).removeClass("go_top");
+		});
+	
 	$(".protfolio .protfolio_intro .mobile").mouseover(function(){
 		var mobile_img = $(this).find("img").height();
 		var plus = $(this).height();
