81e294abbc4e4e1388d8cf3f1781dd9686ce0c25
Judy Ji <judy@aidkr.com>
2024-03-13 09:51:09 +0900

    포트폴리오 2차

--- js/index.js
+++ js/index.js
@@ -1,44 +1,73 @@
-$(document).ready(function(){
+
+//if ($( window ).width() >= 1024) {
+
+  // 포폴 리스트
+  $(function () { // wait for document ready
+	var controller = new ScrollMagic.Controller();
+  
+	var horizontalSlide = new TimelineMax()
+	// animate panels
+	.to("#listContainer", 1,   {x: "-1%"}) 
+	.to("#listContainer", 1,   {x: "-20%"}) 
+	.to("#listContainer", 1,   {x: "-40%"})
+	.to("#listContainer", 1,   {x: "-60%"})
+	.to("#listContainer", 1,   {x: "-80%"})
+  
+	// create scene to pin and link animation
+	new ScrollMagic.Scene({
+	  triggerElement: "#list_trigger",
+	  triggerHook: "onLeave",
+	  duration: "400%"
+	})
+	  .setPin("#list_trigger")
+	  .setTween(horizontalSlide)
+	  //.addIndicators() // add indicators (requires plugin)
+	  .addTo(controller);
 	
-		$('#fullpage').fullpage({
-			responsiveWidth: 1024,
-			'onLeave' : function (index, nextIndex, direction){
-				/* 탑버튼 섹션1에서 숨기기 */
-				if (index == 1 && direction == 'down'){
-					$(".top_btn").addClass("go_top");
-				}else if (index == 2 && direction == 'up'){
-					$(".top_btn").removeClass("go_top");
-				}
-
-				/* 푸터에서 네브 조정 */
-				if (index == 6 && direction == 'down'){
-					$(".top_btn").addClass("ft_on");
-				}else if (index == 7 && direction == 'up'){
-					$(".top_btn").removeClass("ft_on");
-				}
-			}
-
-		});
-		$('.top_btn').on('click', function(){
-			$.fn.fullpage.moveTo(1);
-			$(this).removeClass("go_top");
-		});
 	
-	$(".protfolio .protfolio_intro .mobile").mouseover(function(){
-		var mobile_img = $(this).find("img").height();
-		var plus = $(this).height();
-		var mobile_img_size = mobile_img-plus
-		$(this).find("img").stop().animate({marginTop: "-"+mobile_img_size}, 15000);
-	}).mouseout(function(){
-		$(this).find("img").stop().animate({marginTop: 0}, 800);
-	});
-	$(".protfolio .protfolio_intro .pc").mouseover(function(){
-		var pc_img = $(this).find("img").height();
-		var plus = $(this).height();
-		var pc_img_size = pc_img-plus
-		$(this).find("img").stop().animate({marginTop: "-"+pc_img_size}, 15000);
-	}).mouseout(function(){
-		$(this).find("img").stop().animate({marginTop: 0}, 800);
-	});
+	
+  });
 
-});
+//}
+
+
+// 컨텍트 화살표
+function pathPrepare ($el) {
+  const lineLength = $el[0].getTotalLength();
+  $el.css("stroke-dasharray", lineLength);
+  $el.css("stroke-dashoffset", lineLength);
+}
+
+const $word = $("path#word");
+const $dot = $("path#dot");
+
+// prepare SVG
+pathPrepare($word);
+pathPrepare($dot);
+
+// init controller
+const controller = new ScrollMagic.Controller();
+
+// build tween
+const tween = new TimelineMax()
+  .add(TweenMax.to($word, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
+  .add(TweenMax.to($dot, 0.1, {strokeDashoffset: 0, ease:Linear.easeNone}))  // draw dot for 0.1
+  .add(TweenMax.to("path", 1, {stroke: "#000", ease:Linear.easeNone}), 0);			// change color during the whole thing
+
+// build scene
+const scene = new ScrollMagic.Scene({triggerElement: ".scroll_top", duration: 300, tweenChanges: true})
+			  .setTween(tween)
+			  .addTo(controller);
+
+//다크모드
+document.getElementById('mode_btn').addEventListener("click", function() {
+if(document.querySelector('body').classList.contains('dark-mode')){
+	document.body.classList.remove("dark-mode");
+}else{
+	document.body.classList.add("dark-mode");
+}
+},false);
+
+if(document.querySelector('body').dataset.theme == 'dark'){
+  document.body.classList.add("dark-mode");
+}
